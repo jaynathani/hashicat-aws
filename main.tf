5211df2813f95d69422cf41c05bb10e1e18fc8a5
Jay Nathani <jaynathani@gmail.com>
2021-06-02 17:37:03 +0000

    Added second tag

--- main.tf
+++ main.tf
@@ -133,7 +133,8 @@ resource "aws_instance" "hashicat" {
 
   tags = {
     Name = "${var.prefix}-hashicat-instance",
-    Department = "devops1"
+    Department = "devops1",
+    Billable = "true"
   }
 }
 
